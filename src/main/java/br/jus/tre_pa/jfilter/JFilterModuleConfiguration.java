ce5e116069c701acf37d4081a285d739a89a5719
jcruz <jcruz@tre-pa.jus.br>
2019-05-08 14:30:25 -0300

    Add @AutoConfigurationPackage no módulo

--- src/main/java/br/jus/tre_pa/jfilter/JFilterModuleConfiguration.java
+++ src/main/java/br/jus/tre_pa/jfilter/JFilterModuleConfiguration.java
@@ -1,10 +1,12 @@
 package br.jus.tre_pa.jfilter;
 
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "br.jus.tre_pa.jfilter")
+@AutoConfigurationPackage
 public class JFilterModuleConfiguration {
 
 }
